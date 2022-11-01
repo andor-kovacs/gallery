e29dec59e586884042697760fe3e88b6772a413c
andor-kovacs <kovacsandor86@gmail.com>
2022-11-01 21:36:26 +0100

    shopping cart update

--- order.js
+++ order.js
@@ -1,19 +1,30 @@
+// -----------------------order-------------------------------
 function add() {
-
     var sum = 0;
+    let result = 0;
     for (let i = 3; i < 9; i++) {
         let numbers = parseInt(document.querySelector(`.bk${i}`).value);
-        sum += numbers;
         document.getElementById("answer-pieces").value = sum;
+        sum += numbers;
+        console.log(sum);
+        console.log(numbers);
+        if (numbers > 0) {
+            let prices = parseInt(document.querySelector(`.price${i}`).getAttribute('data-price'));
+            result += numbers * prices;
+            document.getElementById("answer-price").value = result;
+            console.log(result);
+        }
     }
-    var result = 0;
+}
 
-    for (let j = 3; j < 4; j++) {
-        let prices = parseInt(document.querySelector(`.price${j}`).value);
-        console.log(prices)
-        result += prices;
-        document.getElementById("answer-price").value = result;
+//-----------------------hide-navbar-----------------------------
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".navbar").style.top = "0px";
+    } else {
+        document.querySelector(".navbar").style.top = "-1500px";
     }
-
-
+    prevScrollpos = currentScrollPos;
 }
