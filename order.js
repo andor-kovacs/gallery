3e6b23fca9ad8dbd1a9df3404fbeb426988b7c59
andor-kovacs <kovacsandor86@gmail.com>
2022-11-02 20:07:34 +0100

    gallery update

--- order.js
+++ order.js
@@ -2,17 +2,14 @@
 function add() {
     var sum = 0;
     let result = 0;
-    for (let i = 3; i < 9; i++) {
+    for (let i = 3; i <= 9; i++) {
         let numbers = parseInt(document.querySelector(`.bk${i}`).value);
-        document.getElementById("answer-pieces").value = sum;
         sum += numbers;
-        console.log(sum);
-        console.log(numbers);
+        document.getElementById("answer-pieces").value = sum;
         if (numbers > 0) {
             let prices = parseInt(document.querySelector(`.price${i}`).getAttribute('data-price'));
             result += numbers * prices;
             document.getElementById("answer-price").value = result;
-            console.log(result);
         }
     }
 }
